d22a42d62e718f870b79353167cd7ad54f2fa5a7
wbaghdadi20 <wbaghdadi5@gmail.com>
2024-07-20 20:22:52 -0700

    Refactored some code. Added new content to navbar when signed in

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./Navbar.css";
 import NavigateNextOutlinedIcon from "@mui/icons-material/NavigateNextOutlined";
 import NavigateBeforeOutlinedIcon from "@mui/icons-material/NavigateBeforeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import DownloadForOfflineOutlinedIcon from "@mui/icons-material/DownloadForOfflineOutlined";
 import { useAuth } from "./AuthProvider";
 import { getAuthUrl } from "./auth";
 
 function Navbar() {
   const [{ user }, dispatch] = useAuth();
+  // const user = true;
 
   const handleLogin = async () => {
     const authUrl = await getAuthUrl();
@@ -17,21 +20,41 @@ function Navbar() {
 
   return (
     <div className="navbar">
-      <div className="nav-buttons">
-        <NavigateBeforeOutlinedIcon className="btn left" />
-        <NavigateNextOutlinedIcon className="btn" />
-      </div>
-      {!user ? ( // Conditionally render the login and signup buttons
-        <div className="nav-login-signup">
-          <span>Sign Up</span>
-          <button onClick={handleLogin}>
-            <strong>Log in</strong>
-          </button>
+      <div class="navbar-lineOne">
+        <div className="nav-buttons">
+          <NavigateBeforeOutlinedIcon className="nav-btn nav-btn-left" />
+          <NavigateNextOutlinedIcon className="nav-btn" />
         </div>
-      ) : (
-        <div className="nav-hello-user">
-          <span>Hello, {user.display_name}</span>{" "}
-          {/* Replace `display_name` with the correct field from user object */}
+        {!user ? (
+          <div className="nav-login-signup">
+            <span>Sign Up</span>
+            <button onClick={handleLogin}>
+              <strong>Log in</strong>
+            </button>
+          </div>
+        ) : (
+          <div className="nav-loggedIn">
+            <button className="explore-premium-btn">Explore Premium</button>
+            <button className="install-app-btn">
+              <DownloadForOfflineOutlinedIcon className="install-icon" />
+              Install App
+            </button>
+            <div className="bg">
+              <NotificationsOutlinedIcon className="idk" />
+            </div>
+            <div className="bg">
+              {/* replace with first Letter of user.displayname and a bg color as well */}
+              <NotificationsOutlinedIcon className="idk" />
+            </div>
+          </div>
+        )}
+      </div>
+      {user && (
+        <div className="navbar-lineTwo">
+          <button className="navbar-lineTwo-content">All</button>
+          <button className="navbar-lineTwo-content">Music</button>
+          <button className="navbar-lineTwo-content">Podcasts</button>
+          <button className="navbar-lineTwo-content">Audiobooks</button>
         </div>
       )}
     </div>
